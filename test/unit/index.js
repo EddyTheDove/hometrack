d6c5a39459b547e21d436f5775b7d794b2dcb4dc
EddyTheDove <eddy.insearch@gmail.com>
2018-02-07 02:36:15 +1100

    updates

--- test/unit/index.js
+++ test/unit/index.js
@@ -2,8 +2,8 @@ let request = require('supertest')
 import app from '../../src'
 let expect = require('chai').expect
 
-describe('Our application', function() {
-    it('should understand basic mathematical principles', function(done) {
+describe('Home Track Root', function() {
+    it('should show error message when GET root link', function(done) {
         request(app)
         .get('/')
         .expect('Content-Type', /json/)
